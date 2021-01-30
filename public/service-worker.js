83b655095ab04a08c38f4b68018fd794b66bbf50
BHH <brucehenryhayes@yahoo.com>
2021-01-30 16:05:36 -0500

    Fixed typo on line 11 in service-worker.js. THIS IS THE MVP.

--- public/service-worker.js
+++ public/service-worker.js
@@ -8,7 +8,7 @@ const FILES_TO_CACHE = [
     "/icons/icon-192x192.png",
     "/icons/icon-512x512.png",
     "/manifest.webmanifest",
-    "/indexedDb.js"
+    "/indexedDB.js"
 ];
 
 const CACHE_NAME = "static-cache-v2";
